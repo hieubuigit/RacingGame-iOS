6d2503ac4ee264f4cc27175a705c14ed69df45bf
Hay <HayHuynh1999>
2020-07-21 01:42:48 -0700

    Complete screen 2 interface, player cars, scoring and stopping the game.

--- RacingGame/PlayingGameController.swift
+++ RacingGame/PlayingGameController.swift
@@ -9,22 +9,122 @@
 import UIKit
 
 class PlayingGameController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var timer:Timer!
+    var gameIsPause: Bool = false
+    var w:CGFloat!, h:CGFloat!
+    
+    @IBOutlet weak var lblDiem: UILabel!
+    @IBOutlet weak var xe: UIImageView!
+    @IBOutlet weak var xe1: UIImageView!
+    @IBOutlet weak var xe2: UIImageView!
+    @IBOutlet weak var xe3: UIImageView!
+    @IBOutlet weak var coin: UIImageView!
+    @IBOutlet weak var racetrack: UIImageView!
+    
+    // Button Pause Game
+    var isTapped: Bool = false
+    @IBOutlet weak var pauseAndResume: UIButton!
+    
+    @IBAction func buttonControl(_ sender: UIButton) {
+        if isTapped {
+            pauseAndResume.setImage(UIImage(named: "pause_button"), for: .normal)
+            isTapped = false
+            timer = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(ChayXe), userInfo: nil, repeats: true)
+        } else {
+            pauseAndResume.setImage(UIImage(named: "play_button"), for: .normal)
+            isTapped = true
+            timer.invalidate()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Tinh tien khi vuot qua mot xe
+    func chayXeVaTien (object: UIImageView) {
+        object.frame = CGRect(x: object.frame.origin.x, y: object.frame.origin.y + 1, width: object.frame.size.width, height: object.frame.size.height)
     }
-    */
-
+    
+    // Tinh diem khi player's Car vuot qua cac car khac
+    func tinhDiemKhiVuotQuaXe(xe: UIImageView) {
+        if xe.frame.origin.y == h {
+            xe.frame.origin.y = 0 - 135
+            let r:Float = Float(arc4random_uniform(UInt32(w)))
+            xe.frame.origin.x = CGFloat(r)
+            
+            guard let text = self.lblDiem.text else { return }
+            var diem = Int(text)
+            diem = diem! + 1
+            lblDiem.text = String(diem!)
+        }
+    }
+    
+    @objc func ChayXe() {
+        // Racetrack Moving
+        //roadMoving(racetrack: racetrack)
+        
+        // Xuat hien ngau nhien xe va tien
+        chayXeVaTien(object: xe1)
+        chayXeVaTien(object: xe2)
+        chayXeVaTien(object: xe3)
+        chayXeVaTien(object: coin)
+        
+        // Tinh diem khi plyer's vuot qua cac car khac
+        tinhDiemKhiVuotQuaXe(xe: xe1)
+        tinhDiemKhiVuotQuaXe(xe: xe2)
+        tinhDiemKhiVuotQuaXe(xe: xe3)
+        
+        // Xuat hien lai coin khi Player's Car khong cham
+        if coin.frame.origin.y == h {
+            coin.frame.origin.y = 0 - 39
+            let r:Float = Float(arc4random_uniform(UInt32(w)))
+            coin.frame.origin.x = CGFloat(r)
+        }
+        
+        // Kiem tra Player's Car cham vao Car khac
+        if xe1.frame.intersects(xe.frame) || xe2.frame.intersects(xe.frame) || xe3.frame.intersects(xe.frame) {
+            timer.invalidate()
+        }
+        
+        // Tinh diem khi Player's Car cham vao coin (Hieu Bui)
+        if coin.frame.intersects(xe.frame) {
+            coin.frame.origin.y = 0 - 39
+            let r:Float = Float(arc4random_uniform(UInt32(w)))
+            coin.frame.origin.x = CGFloat(r)
+            guard let text = self.lblDiem.text else { return }
+            var diem = Int(text)
+            diem = diem! + 10
+            lblDiem.text = String(diem!)
+        }
+    }
+    
+    // MARK: Move the Player's Car
+    @IBAction func KeoTha(_ sender: UIPanGestureRecognizer) {
+        let tran = sender.translation(in: self.view)
+        sender.view!.center = CGPoint(x: sender.view!.center.x + tran.x, y: sender.view!.center.y)
+        sender.setTranslation(CoreGraphics.CGPoint.zero, in: self.view)
+    }
+    
+    // MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Cho diem gia tri bang 0
+        lblDiem.text = "0"
+        
+        // Lay kich thuot man hinh
+        w = self.view.frame.width
+        h = self.view.frame.height
+        
+        xe1.frame.origin.y = 0 - 600
+        xe2.frame.origin.y = 0 - 200
+        xe3.frame.origin.y = 0 - 400
+        coin.frame.origin.y = 0 - 350
+        
+        // Xac dinh ban dau cua cac doi tuong
+        xe1.frame = CGRect(x: xe1.frame.origin.x, y: xe1.frame.origin.y - 600, width: xe1.frame.size.width, height: xe1.frame.size.height)
+        xe2.frame = CGRect(x: xe2.frame.origin.x, y: xe2.frame.origin.y - 600, width: xe2.frame.size.width, height: xe2.frame.size.height)
+        xe3.frame = CGRect(x: xe3.frame.origin.x, y: xe3.frame.origin.y - 600, width: xe3.frame.size.width, height: xe3.frame.size.height)
+        coin.frame = CGRect(x: coin.frame.origin.x, y: coin.frame.origin.y - 600, width: coin.frame.size.width, height: coin.frame.size.height)
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(ChayXe), userInfo: nil, repeats: true)
+    }
 }
