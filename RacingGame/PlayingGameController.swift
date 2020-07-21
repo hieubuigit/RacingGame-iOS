74c51c618ad53461c384a8e8585ccf091e97ba54
hieubuigit <hieubv1306@gmail.com>
2020-07-21 10:03:04 +0700

    The first commit.

---
+++ RacingGame/PlayingGameController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayingGameController.swift
+//  RacingGame
+//
+//  Created by Hieu Bui on 7/21/20.
+//  Copyright © 2020 project. All rights reserved.
+//
+
+import UIKit
+
+class PlayingGameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
