9020ce0050d57454fcc5d7b2e73c02383d931f0c
hieubuigit <hieubv1306@gmail.com>
2020-07-21 09:42:07 +0700

    Initial Commit

---
+++ RacingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RacingGame
+//
+//  Created by Hieu Bui on 7/21/20.
+//  Copyright © 2020 project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
